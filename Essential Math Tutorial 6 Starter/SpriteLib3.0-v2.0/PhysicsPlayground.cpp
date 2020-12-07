bad8c9b8a710669d29a8a684b0864727f93259a9
Christian Moncada <christian.moncada@ontariotechu.net>
2020-12-07 14:07:53 -0500

    fixed dynamic head

--- Essential Math Tutorial 6 Starter/SpriteLib3.0-v2.0/PhysicsPlayground.cpp
+++ Essential Math Tutorial 6 Starter/SpriteLib3.0-v2.0/PhysicsPlayground.cpp
@@ -1379,7 +1379,7 @@ void PhysicsPlayground::InitScene(float windowWidth, float windowHeight)
 		//tempPhsBody = PhysicsBody(tempBody, float(tempSpr.GetWidth() - shrinkX), float(tempSpr.GetHeight() - shrinkY), vec2(0.f, 0.f), false);
 		//tempPhsBody = PhysicsBody(entity, tempBody, float((tempSpr.GetWidth() - shrinkY) / 2.f), vec2(0.f, 0.f), false, OBJECTS, GROUND | ENVIRONMENT | PLAYER | TRIGGER, 0.3f);
 		std::vector<b2Vec2> points = { b2Vec2(-tempSpr.GetWidth() / 2.f, -tempSpr.GetHeight() / 2.f),b2Vec2(tempSpr.GetWidth() / 2.f,-tempSpr.GetHeight() / 2.f), b2Vec2(tempSpr.GetWidth() / 2.f, tempSpr.GetHeight() / 2.f),b2Vec2(-tempSpr.GetWidth() / 2.f,tempSpr.GetHeight() / 2.f) };
-		tempPhsBody = PhysicsBody(entity, BodyType::CIRCLE, tempBody, points, vec2(0.f, 0.f), false, OBJECTS, ENEMY | PLAYER, 0.5f, 3.f);
+		tempPhsBody = PhysicsBody(entity, BodyType::CIRCLE, tempBody, points, vec2(0.f, 0.f), false, OBJECTS, GROUND | ENEMY | PLAYER, 0.5f, 3.f);
 
 		tempPhsBody.SetColor(vec4(1.f, 0.f, 1.f, 0.3f));
 		tempPhsBody.SetGravityScale(0.f);
